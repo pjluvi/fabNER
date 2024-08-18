f4ef46bf259b425cb9c9640f8d83b7385c8641a4
Prathik Jain <prathik.jain.gn@gmail.com>
2024-08-18 20:25:16 +0530

    Setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Prathik",
+    author_email="prathik.jain.gn@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
